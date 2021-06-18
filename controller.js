5c6ffcaf19c72ce068eda3c56dbb464c27d0d113
Fahmi <gst.alfahmi@gmail.com>
2021-06-18 10:08:56 +0700

    membuat controller delete

--- controller.js
+++ controller.js
@@ -58,3 +58,16 @@ exports.editMahasiswa = (req, res)=> {
         }
     });
 }
+
+//hapus mahasiswa
+exports.hapusMahasiswa = (req, res)=> {
+    var id = req.body.id_mahasiswa;
+
+    connection.query('DELETE FROM mahasiswa WHERE id_mahasiswa=?',[id], (error, rows, fields)=> {
+        if(error){
+            console.log(error); 
+        } else {
+            response.ok("Berhasil Delete Data", res);
+        }
+    });
+}
