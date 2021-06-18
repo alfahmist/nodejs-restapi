6e26874f42ec764b1fc651d15077612b5f28639e
Fahmi <gst.alfahmi@gmail.com>
2021-06-18 08:45:43 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+//panggil routers
+var routes = require('./routes');
+routes(app);
+
+
 let port = 3000;
 app.listen(port, () => {
     console.log(`Server started on port`);
