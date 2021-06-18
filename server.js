4fe9b7a23755e698ab6ea1d384bf22e5ee7c5afb
Fahmi <gst.alfahmi@gmail.com>
2021-06-18 14:01:45 +0700

    Update server.js
    tambahkan router middleware

--- server.js
+++ server.js
@@ -2,17 +2,21 @@ const express = require('express');
 // versi lama less than 4.16.0
 // const bodyParser = require('body-parser');
 
+var morgan = require('morgan');
 const app = express();
 
 
 //parse application/json
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(morgan('dev'));
 
 //panggil routers
 var routes = require('./routes');
 routes(app);
 
+//daftarkan menu routes dari index
+app.use('/auth', require('./middleware'));
 
 let port = 3000;
 app.listen(port, () => {
