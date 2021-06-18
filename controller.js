9bc8e92cf1e81714046b87098adba276980b52f0
Fahmi <gst.alfahmi@gmail.com>
2021-06-18 09:09:50 +0700

    berhasil menampilkan data dari database ke website

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./koneksi');
 exports.index = (req, res) => {
     response.ok("Aplkasi REST API berjalan", res);
 }
+
+//menampilkan semua data mahasiswa
+exports.tampilSemuaMahasiswa = (req, res) => {
+    connection.query('SELECT * FROM mahasiswa', (error, rows, fields)=> {
+        if(error){
+            connection.log(error);
+        } else {
+            response.ok(rows, res);
+        }
+    });
+};
