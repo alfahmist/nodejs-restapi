cbf9e109983fbe777d6199c35d0cda20d9d23d88
Fahmi <gst.alfahmi@gmail.com>
2021-06-18 11:02:19 +0700

    json grouping matakuliah dengan key nama mahasiswa

--- res.js
+++ res.js
@@ -9,3 +9,33 @@ exports.ok = (values, res) => {
     res.json(data);
     res.end();
 }
+
+//response untuk nested matakuliah
+exports.oknested = (values, res)=> {
+    //akumulasi
+    const hasil = values.reduce((akumulasikan, item)=> {
+        //tentukan key groupnya
+        if(akumulasikan[item.nama]){
+            //buat variable group nama mahasiswa
+            const group = akumulasikan[item.nama];
+            //cek jika isi array adalah matakuliah
+            if(Array.isArray(group.matakuliah)){
+                //tambahkan valuenya kedalam group matakuliah
+                group.matakuliah.push(item.matakuliah);
+            } else {
+                group.matakuliah = [group.matakuliah, item.matakuliah]
+            }
+        } else {
+            akumulasikan[item.nama] = item;
+        }
+        return akumulasikan;
+    }, {});
+
+    var data = {
+        'status':200,
+        'values':hasil
+    };
+
+     res.json(data);
+     res.end();
+}
